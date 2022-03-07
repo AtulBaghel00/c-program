aed3badbcc40cc54b26d54a1f97c03ac3f919cc0
Atul Baghel <92114450+AtulBaghel00@users.noreply.github.com>
2022-03-07 19:57:26 +0530

    programm to find sum of two number

---
+++ sum of two number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a,b,c;
+    printf("enter the value of a ");
+    scanf("%d",&a);
+    printf("enter the value of b ");
+    scanf("%d",&b);
+    
+    c=a+b;
+    printf("%d",c);
+
+
+    return 0;
+}
