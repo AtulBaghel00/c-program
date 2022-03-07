62f8d0b4692f162acfec36e8efa75c1f712537f9
Atul Baghel <92114450+AtulBaghel00@users.noreply.github.com>
2022-03-07 19:44:34 +0530

    program to convert celcius to fara temp

---
+++ celcius to fara.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int radius,height;
+    float b=3.14;
+    printf("enter the radius of cyclinder is %d \n");
+    scanf("%d",&radius);
+    printf("enter the height the height of cyclinder is %d \n");
+    scanf("%d", &height);
+    
+    printf("the volume of a cyclinder is %f ", b*radius*radius*height);
+    return 0;
+}
