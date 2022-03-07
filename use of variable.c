2a81bdd1103aead679bcf27ce3eb95b2da12ace2
Atul Baghel <92114450+AtulBaghel00@users.noreply.github.com>
2022-03-07 19:45:16 +0530

    use of variable

---
+++ use of variable.c
@@ -0,0 +1,17 @@
+int main(){
+    int a = 2;
+    int e = 35;
+    float b =7.7;
+    char h= 'i';
+    char c='a';
+    printf( "%d \n",a);
+    printf("the value of ab is %f \n", b);
+    printf("the value of c is %c \n", c);
+    printf("the value of a+e is %d \n", a+e);
+    printf("the value of substraction is %d \n",a-e);
+    printf("the value of multi is %d \n",a*e);
+    printf("the value of division is %d \n",e/a);
+    printf("the value of sum is %c \n",'c+h');
+ 
+    return 0;
+}
