e3b09d39253cf2b4482cf8d37854dd9cdfe84ada
Atul Baghel <92114450+AtulBaghel00@users.noreply.github.com>
2022-03-08 23:42:34 +0530

    program to print num b/w 1to20

---
+++ number between 10 t0 20.c
@@ -0,0 +1,61 @@
+//int main()
+{
+    int i=0;
+    while(i<=20){
+        if (i>=10){
+          printf("the number betweeen 10 to 20 is %d\n",i);
+        }
+        i++;
+    }
+    return 0;
+} 
+
+//by for loop
+
+int main()
+{
+    int i=0;
+    for(i=10 ;i ;i++){
+        printf("number between 10 to 20 is %d \n",i);
+        if(i==20){
+            break;
+        }
+    }
+    return 0;
+}
+
+
+
+// by do while loop
+int main()
+{
+    int i=10;
+    do{
+        printf("number between  10 to 20 is %d \n",i);
+        i++;
+        
+    }while(i<=20);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
