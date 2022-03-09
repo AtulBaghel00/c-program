b78f6a4cc8cd34dca690cb0ccbe529436fe975d4
Atul Baghel <92114450+AtulBaghel00@users.noreply.github.com>
2022-03-09 21:28:01 +0530

    program to read variable from keyboard

---
+++ read_variable_from_keyboar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    char chara;
+    printf("enetr the character ");
+    scanf("%c", &chara);
+    int num;
+    printf("enter the value of num is ");
+    scanf("%d", &num);
+    float a;
+    printf("enter the float value ");
+    scanf("%f", &a);
+     
+
+    printf("\n %d",num);
+    printf("\n %f",a);
+    printf("\n %c",chara);
+    return 0;
+
+}
