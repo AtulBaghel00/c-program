754a61fe948045529035421ee985138e63d5289b
Atul Baghel <92114450+AtulBaghel00@users.noreply.github.com>
2022-03-10 22:42:20 +0530

    multiplication question

--- multiplication.c
+++ multiplication.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
+void printTable(int *mulTable, int num, int n){
+    printf("The multiplication table of %d is :\n", num);
+    for(int i=0; i<n; i++){
+        mulTable[i] = num*(i+1);
+    }
+
+    for(int i=0; i<n; i++){
+        printf("%dX%d = %d\n", num, i+1, mulTable[i]);
+    }
+    printf("******************************************************\n\n");
+}
+
 int main(){
-    int n;
-    printf("enter the value of n :");
-    scanf("%d",&n);
-    int mult[10];
-    for(int i=0;i<10;i++){
-        mult[i]=n*(i+1);
-    }
-    for(int i=0;i<10;i++){
-        printf("%d * %d = %d\n",n,i+1,mult[i]);
-    }
+    
+    int mulTable[3][10];
+    printTable(mulTable[0], 2, 10);
+    printTable(mulTable[1], 7, 10);
+    printTable(mulTable[2], 9, 10);
 
-
+    return 0;
 }
